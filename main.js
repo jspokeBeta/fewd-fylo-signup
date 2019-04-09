61d8e85339f77eaeb6c52591627f95e5855572d1
jspokeBeta <47574993+jspokeBeta@users.noreply.github.com>
2019-04-09 19:07:44 -0400

    Alert confirmation on email submission

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 //Get email input field
 var emailInput = document.querySelector('#email');
+
+// Get submit button
+ var submitBtn = document.querySelector('[type="submit"]');
+
+// Get user input and return an alert message
+
+  function emailConfirmation(event) {
+    //Bail if click is not on Submit button
+    if (event.target !== submitBtn) return;
+    //Get user email address
+    var userEmail = emailInput.value;
+
+    //Show alert confirmation message
+    alert(`Your email address has been successfully submitted: ${userEmail}`);
+
+    //Prevent default
+    event.preventDefault();
+  }
+
+//Listen for clicks on document
+  document.addEventListener('click', emailConfirmation, false);
